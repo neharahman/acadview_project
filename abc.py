2abb09f40b66552f85d24caec0628d8e46f708f0
Shubham raj <shubhamrajabc@gmail.com>
2018-06-06 16:30:37 +0530

    my first commit

---
+++ abc.py
@@ -0,0 +1,18 @@
+#question 1
+print("Hello My name is Neha Rahman")
+#question 2
+print("good"+"morning")
+#question 3
+x=23
+#estion 4
+print("Let's get started")
+#question 5
+s="acadview"
+course="python"
+fees=5000
+print("%s%s%d",s,course,fees)
+#question 6
+name="Tony Stark"
+salary=100000
+print("%s%d")%(____,____)
+
